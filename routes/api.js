8a73c70fa9d24157521fd685d003012d2f8ed0ac
zafajardo9 <zafajardo9.programming@gmail.com>
2023-08-07 22:07:47 +0800

    first

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+ var expect = require('chai').expect;
+ let mongodb = require('mongodb');
+ let mongoose = require('mongoose');
+
+module.exports = function (app) {
+
+  app.route('/api/stock-prices')
+    .get(function (req, res){
+      
+    });
+    
+};
